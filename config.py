e6fbe2125ffd8f2ad5664ceb5b8156c4129693b9
CvaTricks <68840422+cvatricks@users.noreply.github.com>
2025-12-07 13:08:55 +0530

    Add bot configuration settings in config.py
    Added configuration settings for multiple bots including API IDs, hashes, and tokens.

---
+++ config.py
@@ -0,0 +1,29 @@
+LOG_CHANNEL_ID = -123234234296 # <-- !!! SET THIS !!!
+
+# 2. Add all your bot credentials here
+BOT_CONFIGS = [
+    {
+        "session_name": "bot1_session",
+        "api_id": 142342,         # <-- Bot 1 API ID
+        "api_hash": "a15sdfadsfasdfasdfasdfs02e413b1", # <-- Bot 1 API Hash
+        "bot_token": "1sdfasdfasdfasfNB4LJdDRXxY2a7U_bF87FsyQE"    # <-- Bot 1 Token
+    },
+    {
+        "session_name": "bot2_session",
+        "api_id": 13423488,         # <-- Bot 2 API ID
+        "api_hash": "35sdfasdfasfe8079e777277cf", # <-- Bot 2 API Hash
+        "bot_token": "54632asdfasdfdsafak2znq9o5XFdnn2gcWuMY0"    # <-- Bot 2 Token
+    },
+    {
+        "session_name": "bot3_session",
+        "api_id": 34342238,         # <-- Bot 2 API ID
+        "api_hash": "dfgsdfgsdfgsfda1e9fe2e55b301cae", # <-- Bot 2 API Hash
+        "bot_token": "gsdfgsdfgdsgfsfYwlsnvpB-pPTX8pE1Gs22vpSPQ"    # <-- Bot 2 Token
+    },
+    {
+        "session_name": "bot4_session",
+        "api_id": 2323118,         # <-- Bot 1 API ID
+        "api_hash": "dfgdfgdsfgsfgs5da1e9fe2e55b301cae", # <-- Bot 1 API Hash
+        "bot_token": "sfgsfgsfgsdV8x1Nn0M7uuOpdjYCXOygc9vOTYSU"    # <-- Bot 1 Token
+    }
+]
